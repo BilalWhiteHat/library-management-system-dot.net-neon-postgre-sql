24ce1de11e1538da1b4fe48b05f47cd2d3c3071e
muhammadbilalkhan <muhammadbilalkhan122@gmail.com>
2025-04-20 15:03:46 +0500

    Added Member CRUD functionality

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -1,13 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using library_management.Data;
 using library_management.Models;
 
 namespace library_management.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MemberController : ControllerBase
+    public class MemberController : Controller
     {
         private readonly NeondbContext _context;
 
@@ -16,76 +19,134 @@ namespace library_management.Controllers
             _context = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Member>>> GetMembers()
+        // GET: Member
+        public async Task<IActionResult> Index()
         {
-            return await _context.Members.ToListAsync();
+            return View(await _context.Members.ToListAsync());
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Member>> GetMember(int id)
+        // GET: Member/Details/5
+        public async Task<IActionResult> Details(int? id)
         {
-            var member = await _context.Members.FindAsync(id);
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            var member = await _context.Members
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (member == null)
             {
                 return NotFound();
             }
 
-            return member;
+            return View(member);
         }
 
-        [HttpPost]
-        public async Task<ActionResult<Member>> PostMember(Member member)
+        // GET: Member/Create
+        public IActionResult Create()
         {
-            _context.Members.Add(member);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetMember), new { id = member.Id }, member);
+            return View();
         }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMember(int id, Member member)
+        // POST: Member/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,PhoneNumber,RegistrationDate")] Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(member);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(member);
+        }
+
+        // GET: Member/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Members.FindAsync(id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+            return View(member);
+        }
+
+        // POST: Member/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,PhoneNumber,RegistrationDate")] Member member)
         {
             if (id != member.Id)
             {
-                return BadRequest();
+                return NotFound();
             }
 
-            _context.Entry(member).State = EntityState.Modified;
-
-            try
+            if (ModelState.IsValid)
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MemberExists(id))
+                try
                 {
-                    return NotFound();
+                    _context.Update(member);
+                    await _context.SaveChangesAsync();
                 }
-                else
+                catch (DbUpdateConcurrencyException)
                 {
-                    throw;
+                    if (!MemberExists(member.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
                 }
+                return RedirectToAction(nameof(Index));
             }
-
-            return NoContent();
+            return View(member);
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMember(int id)
+        // GET: Member/Delete/5
+        public async Task<IActionResult> Delete(int? id)
         {
-            var member = await _context.Members.FindAsync(id);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Members
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (member == null)
             {
                 return NotFound();
             }
 
-            _context.Members.Remove(member);
-            await _context.SaveChangesAsync();
+            return View(member);
+        }
 
-            return NoContent();
+        // POST: Member/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var member = await _context.Members.FindAsync(id);
+            if (member != null)
+            {
+                _context.Members.Remove(member);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
         private bool MemberExists(int id)
